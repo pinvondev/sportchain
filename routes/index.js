42313deafb3223d698982b909f79106d2d82a215
pinvondev <pinvondev@gmail.com>
2018-06-01 08:59:58 +0800

    <update><routes/index.js><修改首页无session时的bug>

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req.session);
-  if (req.session.user) {
+  if (req.session === undefined) {
+    res.render('index', { title: '首页', username: '请登录'});
+  } else if (req.session.user) {
     res.render('index', { title: 'Express', username: req.session.user.name});
   } else {
     res.render('index', { title: 'Express', username: '请登录' });
