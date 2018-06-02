94bc989dbce727a6034c05fc80ad031a9265ffa0
pinvondev <pinvondev@gmail.com>
2018-06-02 21:38:24 +0800

    <add><增加根据商家名来删除活动>

--- routes/shop.js
+++ routes/shop.js
@@ -1,44 +1,80 @@
 var express = require('express');
 var sql = require('../dao/dao');
+var utils = require('../util/util');
 var router = express.Router();
 
 /* GET shop page. */
 router.get('/', function(req, res, next) {
-    sql.queryByName('shops', [req.session.user.name], function (error, results) {
+    if (req.session === undefined || req.session.user === undefined) {
+        console.log('pinvon', 'undefined');
+        res.redirect('../users/login');
+    } else {
+        sql.queryByName('shops', [req.session.user.name], function (error, results) {
+            if (error) {
+                throw error;
+            } else {
+                console.log(results);
+                shopId = results.id;
+                res.render('shop/', { shop: results });
+            }
+        });
+    }
+});
+
+router.get('/activity', function(req, res, next) {
+    sql.queryByName('activity', [req.session.user.name], function (error, result) {
         if (error) {
             throw error;
+        } else if (result.length === 0) {// 如果未设置活动
+            console.log('pinvon', 'get activity', result);
+            res.render('shop/activity');
+        } else if (result[0].endTime < Date.parse(new Date())) {// 如果活动时间已过, 则从表中删除该活动记录
+            console.log('pinvon', 'timeout');
+            sql.deleteByName('activity', [req.session.user.name], function (error, result) {
+                if (error) {
+                    throw error;
+                } else {
+                    res.render('shop/activity');
+                }
+            });
         } else {
-            console.log(results);
-            shopId = results.id;
-            res.render('shop/', { shop: results });
+            // 如果已经设置过活动时间, 则显示活动细节
+            beginTime = utils.timestampToDate(result[0].beginTime);
+            result[0].beginTime = beginTime;
+
+            endTime = utils.timestampToDate(result[0].endTime);
+            result[0].endTime = endTime;
+            console.log('pinvon', result[0]);
+            res.render('shop/activity', {result: result[0]});
         }
     });
 });
 
-router.get('/activity', function(req, res, next) {
-    res.render('shop/activity');
-});
-
 router.post('/activity', function(req, res, next) {
     console.log(req.body);
-    if (req.body.flag === 'submit') {
-        params = [
-            req.session.user.name,
-            0,
-            req.body.nBeginTime,
-            req.body.nEndTime,
-            req.body.nCoupons,
-            req.body.nCoupons,
-            req.body.fDiscount,
-            req.body.sUrl
-        ]
-    }
+    params = [
+        req.session.user.name,
+        0,
+        req.body.nBeginTime,
+        req.body.nEndTime,
+        req.body.nCoupons,
+        0,
+        req.body.fDiscount,
+        req.body.sUrl,
+        req.body.nEnergy
+    ]
+
     console.log('pinvon', params);
     sql.insert('activity', params, function(error, result){
         if (error) {
             throw error;
         } else {
             console.log(result);
+            response = {
+                code: 200
+            }
+            res.json(response);
+            return ;
         }
     });
 });
